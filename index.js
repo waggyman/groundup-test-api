1d118d8e0441776766736dc4f374318527eac13b
Arigi Wiratama <arigi@ofload.com>
2023-03-12 21:28:18 +0700

    Close connection after call

--- index.js
+++ index.js
@@ -14,41 +14,24 @@ app.use(cors({
 app.use(bodyParser.json())
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }))
 app.use(bodyParser.text({ type: 'text/html' }))
-let connection, collection
-
-// (async () => {
-//     try {
-//         connection = await MongoClient.connect("mongodb+srv://root:Test1234@superlevi.tta6sbs.mongodb.net/groundup_test", {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         })
-//         const db = connection.db('groundup_test')
-//         collection = db.collection('anomalies')
-//         console.log("CONNECTED")
-//     } catch (error) {
-//         console.log("ADA ERROR ON CONNECTING MONGO",  error)
-//         throw error
-//     }
-// })()
-// ADDING MONGO CLIENT IN GET AND PATCH REQUEST BECAUSE IT'S SERVERLESS IN VERCEL
 app.get('/', async (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/anomalies', async (req, res) => {
     try {
-        connection = await MongoClient.connect(`${process.env.MONGO_URL}`, {
+        const connection = await MongoClient.connect(`${process.env.MONGO_URL}`, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
         const db = connection.db('groundup_test')
-        collection = db.collection('anomalies')
+        const collection = db.collection('anomalies')
         const cursor = collection.find({});
         const anomalies = []
         await cursor.forEach((data) => {
             anomalies.push(data)
         })
-
+        connection.close()
         res.send(anomalies)
     } catch (error) {
         console.log("ERROR ON GETTING ANOMALIES", error)
@@ -63,12 +46,12 @@ app.get('/audio/:name', (req, res) => {
 })
 
 app.post('/anomalies/:id', async (req, res) => {
-    connection = await MongoClient.connect(`${process.env.MONGO_URL}`, {
+    const connection = await MongoClient.connect(`${process.env.MONGO_URL}`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     const db = connection.db('groundup_test')
-    collection = db.collection('anomalies')
+    const collection = db.collection('anomalies')
     const {suspectedReason, action, comment} = req.body
     const found = await collection.findOne({_id: new ObjectId(req.params.id)})
     if (!found) throw Error("Cannot found")
@@ -80,6 +63,7 @@ app.post('/anomalies/:id', async (req, res) => {
                 comment
             }
         })
+        connection.close()
         res.send("Update")
     } catch (error) {
         throw error
